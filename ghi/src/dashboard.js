e79694df7b49c487203f0fba21f04f8c8a659a3d
Boden <boden.bradley@gmail.com>
2023-01-13 14:37:19 -0800

    adjust request info link

--- ghi/src/dashboard.js
+++ ghi/src/dashboard.js
@@ -124,7 +124,7 @@ class WineryList extends React.Component {
               className="d-flex justify-content-center btn btn-lg px-4 gap-3 text-light"
               style={{ backgroundColor: 'mediumorchid' }}
             >
-              Request More Info
+              Request More Info About Our Services
             </Link>
           </h5>
         </div>
