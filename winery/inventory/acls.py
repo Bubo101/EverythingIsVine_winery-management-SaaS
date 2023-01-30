6ba542414f2982fb36bbc58887303c93f743d36f
Boden <boden.bradley@gmail.com>
2023-01-30 15:53:45 -0800

    edited 3rd party api call in backend to save previous searches for faster loading

--- winery/inventory/acls.py
+++ winery/inventory/acls.py
@@ -4,9 +4,14 @@ import requests
 
 GEO_API_KEY = os.environ["GEO_API_KEY"]
 
+localCache = {}
 
 def get_geo(address):
     # headers = {"Authorization": }
+
+    if address in localCache:
+        return localCache[address]
+
     params = {
         "access_key": GEO_API_KEY,
         "query": f"{address}",
@@ -14,6 +19,12 @@ def get_geo(address):
     url = "http://api.positionstack.com/v1/forward"
     response = requests.get(url, params=params)
     content = json.loads(response.content)
+
+    if response.ok:
+        localCache[address] = {
+            "latitude": content["data"][0]["latitude"], 
+            "longitude": content["data"][0]["longitude"],
+        }
     try:
         return {
             "latitude": content["data"][0]["latitude"],
