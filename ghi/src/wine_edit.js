e275f5025fdfee8f95e4c7a0189684f393d531d0
Boden <boden.bradley@gmail.com>
2023-03-30 05:30:35 -0700

    updated wine edit page

--- ghi/src/wine_edit.js
+++ ghi/src/wine_edit.js
@@ -119,7 +119,7 @@ function EditWine() {
   return (
     <>
       <div className={'wrapper fadeInDown' + (staff ? ' d-none' : '')}>
-        Sorry, you are not authorized to view this page
+        Sorry, you are not authorized to view this page, go back!
       </div>
       <div className={'wrapper fadeInDown' + (staff ? '' : ' d-none')}>
         <div id="formContent" className="fadeIn first">
